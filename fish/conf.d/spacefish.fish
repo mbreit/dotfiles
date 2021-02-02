e1640cd2c60566fdb5fb546f7c05048b22f6db4b
Moritz Breit <mail@moritz-breit.de>
2021-02-02 11:07:50 +0100

    Add configuration for spacefish prompt

---
+++ fish/conf.d/spacefish.fish
@@ -0,0 +1,11 @@
+if status --is-interactive
+    set SPACEFISH_PROMPT_ORDER time user dir host git ruby rust exec_time line_sep vi_mode jobs exit_code char
+
+    set SPACEFISH_PROMPT_FIRST_PREFIX_SHOW true
+    set SPACEFISH_VI_MODE_PREFIX ""
+    set SPACEFISH_VI_MODE_SUFFIX ""
+    set SPACEFISH_VI_MODE_INSERT " "
+    set SPACEFISH_VI_MODE_NORMAL "N"
+    set SPACEFISH_VI_MODE_VISUAL "V"
+    set SPACEFISH_CHAR_PREFIX ""
+end

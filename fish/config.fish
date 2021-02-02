bf2989626f5059c77d4b8eba786fc9206f70f58f
Moritz Breit <mail@moritz-breit.de>
2021-02-02 11:21:50 +0100

    Extract fundle config in its on plugins.fish file

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,2 @@
-if not functions -q fundle
-    eval (curl -sfL https://git.io/fundle-install)
-end
-
-fundle plugin 'mbreit/spacefish' --url 'git@github.com:mbreit/spacefish.git#patch-1'
-fundle plugin 'evanlucas/fish-kubectl-completions'
-fundle plugin 'brgmnn/fish-docker-compose'
-fundle plugin 'jethrokuan/z'
-fundle plugin 'franciscolourenco/done'
-fundle plugin 'laughedelic/pisces'
-fundle plugin 'oh-my-fish/plugin-bang-bang'
-
-fundle init
-
 set -g fish_greeting ""
 set -g EDITOR vim

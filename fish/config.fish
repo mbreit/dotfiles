e9780a97b5320ed128fb6414187f875a8dddf24b
Moritz Breit <mail@moritz-breit.de>
2021-02-02 09:58:07 +0100

    Add more fish plugins: z, done, pisces and bang-bang

--- fish/config.fish
+++ fish/config.fish
@@ -5,5 +5,9 @@ end
 fundle plugin 'matchai/spacefish'
 fundle plugin 'evanlucas/fish-kubectl-completions'
 fundle plugin 'brgmnn/fish-docker-compose'
+fundle plugin 'jethrokuan/z'
+fundle plugin 'franciscolourenco/done'
+fundle plugin 'laughedelic/pisces'
+fundle plugin 'oh-my-fish/plugin-bang-bang'
 
 fundle init

3dca92eb164e80c6bf4a2952784c6c8b59a2425b
Moritz Breit <mail@moritz-breit.de>
2021-02-02 11:04:53 +0100

    Load fish vi bindings only in interactive shells

--- fish/conf.d/vi.fish
+++ fish/conf.d/vi.fish
@@ -1,19 +1,21 @@
-function fish_user_key_bindings
-    # Execute this once per mode that emacs bindings should be used in
-    fish_default_key_bindings -M insert
-    # Without an argument, fish_vi_key_bindings will default to
-    # resetting all bindings.
-    # The argument specifies the initial mode (insert, "default" or visual).
-    fish_vi_key_bindings insert
-end
+if status --is-interactive
+    function fish_user_key_bindings
+        # Execute this once per mode that emacs bindings should be used in
+        fish_default_key_bindings -M insert
+        # Without an argument, fish_vi_key_bindings will default to
+        # resetting all bindings.
+        # The argument specifies the initial mode (insert, "default" or visual).
+        fish_vi_key_bindings insert
+    end
 
-# Emulates vim's cursor shape behavior
-# Set the normal and visual mode cursors to a block
-set fish_cursor_default block
-# Set the insert mode cursor to a line
-set fish_cursor_insert line
-# Set the replace mode cursor to an underscore
-set fish_cursor_replace_one underscore
-# The following variable can be used to configure cursor shape in
-# visual mode, but due to fish_cursor_default, is redundant here
-set fish_cursor_visual block
+    # Emulates vim's cursor shape behavior
+    # Set the normal and visual mode cursors to a block
+    set fish_cursor_default block
+    # Set the insert mode cursor to a line
+    set fish_cursor_insert line
+    # Set the replace mode cursor to an underscore
+    set fish_cursor_replace_one underscore
+    # The following variable can be used to configure cursor shape in
+    # visual mode, but due to fish_cursor_default, is redundant here
+    set fish_cursor_visual block
+end

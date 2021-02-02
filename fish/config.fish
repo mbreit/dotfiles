20a53cd83d7a853d1cf22db1a70e8c60dbf79027
Moritz Breit <mail@moritz-breit.de>
2021-02-02 09:58:40 +0100

    Move to my own spacefish fork to fix ruby version in prompt with asdf

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ if not functions -q fundle
     eval (curl -sfL https://git.io/fundle-install)
 end
 
-fundle plugin 'matchai/spacefish'
+fundle plugin 'mbreit/spacefish' --url 'git@github.com:mbreit/spacefish.git#patch-1'
 fundle plugin 'evanlucas/fish-kubectl-completions'
 fundle plugin 'brgmnn/fish-docker-compose'
 fundle plugin 'jethrokuan/z'

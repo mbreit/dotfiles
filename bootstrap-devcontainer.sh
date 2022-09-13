46b87e5a28d506075057d90346f23c181bfd806e
Moritz Breit <mail@moritz-breit.de>
2022-09-13 16:09:42 +0200

    Update bootstrap-devcontainer.sh
    - Fix ownership of ~/.local
    - Install htop and procps
    - Initialize fundle and install plugins

--- bootstrap-devcontainer.sh
+++ bootstrap-devcontainer.sh
@@ -3,6 +3,16 @@
 # Bootstrap a devcontainer environment
 # Only supports debian/ubuntu for now and expects passwordless sudo
 
+mkdir -p ~/.local ~/.config
+# Fix ownership for ~/.local or ~/.config in case it is a docker volume
+sudo chown -R $(id -u):$(id -g) ~/.local ~/.config
+
+# Install debian packages
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends openssh-client less fish neovim rake
+sudo apt-get install -y --no-install-recommends procps htop openssh-client less fish neovim rake
+
+# Install dotfiles
 rake install
+
+# Initialize fundle for fish plugins
+fish -i -c "source fish/conf.d/plugins.fish && fundle install"

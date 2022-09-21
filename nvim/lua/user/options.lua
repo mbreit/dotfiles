b2e6eb03ce6e505a863c9ba9419d3ed645e768e0
Moritz Breit <mail@moritz-breit.de>
2022-09-21 18:35:29 +0200

    Set confirm to ask to write on quit

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,4 +1,5 @@
 vim.opt.backup = false                          -- creates a backup file
+vim.opt.confirm = true                          -- ask to write unsaved buffers on exit
 vim.opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
 vim.opt.cmdheight = 1                           -- more space in the neovim command line for displaying messages
 vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp

34c2e4735b0aeff5eda28f7bbe94450717c4112b
Moritz Breit <mail@moritz-breit.de>
2022-09-22 08:52:01 +0200

    Install and configure nvim in bootstrap-devcontainer.sh

--- bootstrap-devcontainer.sh
+++ bootstrap-devcontainer.sh
@@ -9,12 +9,16 @@ sudo chown -R $(id -u):$(id -g) ~/.local ~/.config
 
 # Install debian packages
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends procps htop openssh-client less fish
+sudo apt-get install -y --no-install-recommends procps htop openssh-client less fish wget
+
+wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
+sudo dpkg -i nvim-linux64.deb
 
 sudo sh -c "echo /usr/bin/fish >> /etc/shells"
 
 # Install dotfiles
 mkdir -p ~/.config
 ln -sTf $(pwd)/fish ~/.config/fish
+ln -sTf $(pwd)/nvim ~/.config/nvim
 
 # fish -ic "fundle install"

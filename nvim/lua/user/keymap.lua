1958e98631ea8b6131c186bb6a3889d14e9aa5b5
Moritz Breit <mail@moritz-breit.de>
2022-09-23 18:13:05 +0200

    Add some more neovim keybindings

--- nvim/lua/user/keymap.lua
+++ nvim/lua/user/keymap.lua
@@ -10,6 +10,19 @@ vim.g.mapleader = " "
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
+-- Easier terminal mode
+keymap("t", "<C-w><Up>", "<C-\\><C-N><C-w><Up>", opts)
+keymap("t", "<C-w><Down>", "<C-\\><C-N><C-w><Down>", opts)
+keymap("t", "<C-w><Left>", "<C-\\><C-N><C-w><Left>", opts)
+keymap("t", "<C-w><Right>", "<C-\\><C-N><C-w><Right>", opts)
+-- Tab to toggle terminal
+keymap("t", "<A-/>", "<C-\\><C-N><C-w><Up>", opts)
+keymap("n", "<A-/>", ":ToggleTerm<cr>", opts)
+
+-- Exit insert mode and save on <C-s>
+keymap("i", "<C-s>", "<Esc>:w<cr>", opts)
+keymap("n", "<C-s>", ":w<cr>", opts)
+
 local status_ok, wk = pcall(require, "which-key")
 if not status_ok then
   return

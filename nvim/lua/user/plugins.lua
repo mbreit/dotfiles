c2a9513f6f5771ca2076235f777115625d1b1081
Moritz Breit <mail@moritz-breit.de>
2022-09-23 13:17:36 +0200

    Add telescope-fzf-native

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -256,6 +256,28 @@ return require('packer').startup(function(use)
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
     requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('telescope').setup {
+        extensions = {
+          fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+          },
+        },
+      }
+    end
+  }
+
+  use {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    after = 'telescope.nvim',
+    run = 'make',
+    config = function()
+      require('telescope').load_extension('fzf')
+    end
   }
 
   use {

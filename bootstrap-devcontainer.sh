a7721d6d0479aa0f4aa0b353a85cabab48dcff73
Moritz Breit <mail@moritz-breit.de>
2022-09-25 16:42:03 +0200

    Update bootstrap-devcontainer.sh
    - Use tempfile for neovim download
    - Download neovim only if it is not already installed
    - Add tmux
    - Bootstrap fish plugins
    - Bootstrap neovim plugins

--- bootstrap-devcontainer.sh
+++ bootstrap-devcontainer.sh
@@ -9,14 +9,24 @@ sudo chown -R $(id -u):$(id -g) ~/.local ~/.config
 
 # Install debian packages
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends procps htop openssh-client less fish wget stow ca-certificates git build-essential curl fzf
+sudo apt-get install -y --no-install-recommends procps htop openssh-client less fish wget stow ca-certificates git build-essential curl fzf tmux
 
-wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
-sudo dpkg -i nvim-linux64.deb
+if [ ! -f /usr/bin/fish ]                                                                                                                                                                   
+then    
+  TEMP_FILE=$(mktemp)    
+  curl -Lo "$TEMP_FILE" https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb    
+  sudo dpkg -i "$TEMP_FILE"    
+  rm "$TEMP_FILE"    
+  sudo sh -c "echo /usr/bin/fish >> /etc/shells"    
+fi
 
-sudo sh -c "echo /usr/bin/fish >> /etc/shells"
+sudo chsh -s /usr/bin/fish $(whoami)
 
 # Install dotfiles
-stow fish nvim
+stow fish nvim tmux
 
-# fish -ic "fundle install"
+./bootstrap-nvim.sh
+
+# Run fundle install twice, plugins are installed in second call
+fish -ic "fundle install"
+fish -ic "fundle install"

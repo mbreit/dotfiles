b00ed40752834b08b5423c71fd1f24019da77ae5
Moritz Breit <mail@moritz-breit.de>
2022-10-26 12:10:21 +0200

    Update nvim to 0.8.0 in bootstrap-devcontainer.sh

--- bootstrap-devcontainer.sh
+++ bootstrap-devcontainer.sh
@@ -11,13 +11,13 @@ sudo chown -R $(id -u):$(id -g) ~/.local ~/.config ~/.cache
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends procps htop openssh-client less fish wget stow ca-certificates git build-essential curl fzf tmux ripgrep
 
-if [ ! -f /usr/bin/nvim ]                                                                                                                                                                   
-then    
-  TEMP_FILE=$(mktemp)    
-  curl -Lo "$TEMP_FILE" https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb    
-  sudo dpkg -i "$TEMP_FILE"    
-  rm "$TEMP_FILE"    
-  sudo sh -c "echo /usr/bin/fish >> /etc/shells"    
+if [ ! -f /usr/bin/nvim ]
+then
+  TEMP_FILE=$(mktemp)
+  curl -Lo "$TEMP_FILE" https://github.com/neovim/neovim/releases/download/v0.8.0/nvim-linux64.deb
+  sudo dpkg -i "$TEMP_FILE"
+  rm "$TEMP_FILE"
+  sudo sh -c "echo /usr/bin/fish >> /etc/shells"
 fi
 
 sudo chsh -s /usr/bin/fish $(whoami)

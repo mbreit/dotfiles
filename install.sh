9466969a099793223b409f06aa052a430084870f
Moritz Breit <mail@moritz-breit.de>
2023-09-01 15:36:06 +0200

    Autoformat install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Replace stdin with /dev/null to fix blocking `isatty` call in fish when used
 # in VSCode devcontainer
-stow fish && ./bootstrap-fish.fish < /dev/null
+stow fish && ./bootstrap-fish.fish </dev/null
 stow git
 stow nvim && ./bootstrap-nvim.sh
 stow tmux

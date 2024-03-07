929a8996c5457a7c3dc8a3f1d0ff75a24896f989
Moritz Breit <mail@moritz-breit.de>
2024-03-07 20:42:58 +0100

    Remove outdated comment about nvim bootstrapping

--- install.sh
+++ install.sh
@@ -4,6 +4,5 @@
 # in VSCode devcontainer
 stow fish && ./bootstrap-fish.fish </dev/null
 stow git
-# Do not run bootstrap-nvim.sh for now as it does not work on older Neovim versions
 stow nvim && ./bootstrap-nvim.sh
 stow tmux

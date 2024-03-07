2b781f62673f0cac01547bc284fd3f178ea0dbda
Moritz Breit <mail@moritz-breit.de>
2024-03-07 20:25:05 +0100

    Update neogit config and add shortcut to open neogit in a split window

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -1,10 +1,15 @@
 return {
   {
     "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "nvim-telescope/telescope.nvim", -- optional
+      "sindrets/diffview.nvim", -- optional
+    },
     keys = {
       { "<leader>gn", "<cmd>Neogit<cr>", desc = "Neogit" },
+      { "<leader>gN", "<cmd>Neogit kind=auto<cr>", desc = "Neogit Split" },
     },
-    dependencies = "nvim-lua/plenary.nvim",
     config = true,
   },
 }
